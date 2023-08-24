5c12f6adf94a56d78b4c4cd4c6a9b59fb8bd30ca
mnunezsa <mnunezsa95@gmail.com>
2023-08-24 17:08:46 -0400

    add date conditionals

--- script.js
+++ script.js
@@ -13,10 +13,10 @@ const account1 = {
     "2019-12-23T07:42:02.383Z",
     "2020-01-28T09:15:04.904Z",
     "2020-04-01T10:17:24.185Z",
-    "2020-05-08T14:11:59.604Z",
-    "2020-05-27T17:01:17.194Z",
-    "2020-07-11T23:36:17.929Z",
-    "2020-07-12T10:51:36.790Z",
+    "2023-08-20T14:11:59.604Z",
+    "2023-08-22T17:01:17.194Z",
+    "2023-08-22T23:36:17.929Z",
+    "2023-08-23T10:51:36.790Z",
   ],
   currency: "EUR",
   locale: "pt-PT", // de-DE
@@ -92,10 +92,7 @@ const displayMovements = function (acc, sort = false) {
   movs.forEach(function (movement, index) {
     const movementType = movement > 0 ? "deposit" : "withdrawal";
     const date = new Date(acc.movementsDates[index]);
-    const day = `${date.getDate()}`.padStart(2, 0);
-    const month = `${date.getMonth() + 1}`.padStart(2, 0);
-    const year = date.getFullYear();
-    const displayDate = `${day}/${month}/${year}`;
+    const displayDate = formatMovementDate(date);
 
     const html = `
       <div class="movements__row">
@@ -249,3 +246,20 @@ btnSort.addEventListener("click", (evt) => {
   displayMovements(currentAccount.movements, !sorted);
   sorted = !sorted;
 });
+
+const formatMovementDate = (date) => {
+  const calcDaysPassed = (date1, date2) => {
+    return Math.round(Math.abs((date2 - date1) / (1000 * 60 * 60 * 24)));
+  };
+  const daysPassed = calcDaysPassed(new Date(), date);
+  console.log(daysPassed);
+  if (daysPassed === 0) return "Today";
+  if (daysPassed === 1) return "Yesterday";
+  if (daysPassed <= 7) return `${daysPassed} days ago`;
+  else {
+    const month = `${date.getMonth() + 1}`.padStart(2, 0);
+    const year = date.getFullYear();
+    const day = `${date.getDate()}`.padStart(2, 0);
+    return `${day}/${month}/${year}`;
+  }
+};
